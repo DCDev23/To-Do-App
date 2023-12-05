0c4974b03deaa08b56af4efc156ba1632c3272bf
DCDev23 <derrick.a.chan@outlook.com>
2023-12-05 09:12:28 +0200

    to do update

--- ToDoScript.js
+++ ToDoScript.js
@@ -1,4 +1,5 @@
 // Function to add a new task with due date to the list
+// Function to add a new task with due date to the list
 function addTodo() {
   const todoInput = document.getElementById('todo-input');
   const dueDateInput = document.getElementById('due-date-input');
@@ -6,22 +7,30 @@ function addTodo() {
   const dueDate = dueDateInput.value.trim();
   
   if (todoText !== '' && dueDate !== '') {
-    const todoList = document.getElementById('todo-list');
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `
-      <span>${todoText}</span>
-      <span class="due-date">Due: ${dueDate}</span>
-      <button onclick="deleteTodo(this)">Delete</button>
-    `;
-    todoList.appendChild(listItem);
-    todoInput.value = '';
-    dueDateInput.value = '';
-    
-    saveTasks(); // Save tasks to localStorage
+    const currentDate = new Date(); // Get the current date
+    const selectedDate = new Date(dueDate); // Convert dueDate to a Date object
+
+    if (selectedDate >= currentDate) {
+      const todoList = document.getElementById('todo-list');
+      const listItem = document.createElement('li');
+      listItem.innerHTML = `
+        <span>${todoText}</span>
+        <span class="due-date">Due: ${dueDate}</span>
+        <button onclick="deleteTodo(this)">Delete</button>
+      `;
+      todoList.appendChild(listItem);
+      todoInput.value = '';
+      dueDateInput.value = '';
+      
+      saveTasks(); // Save tasks to localStorage
+    } else {
+      alert('Please select a due date on or after the present day!');
+    }
   } else {
     alert('Please enter a task and due date!');
   }
 }
+
 
 // Function to delete a task from the list
 function deleteTodo(element) {
@@ -64,3 +73,18 @@ function loadTasks() {
     todoList.innerHTML = savedTasks;
   }
 }
+
+// Function to change screen orientation
+function changeScreenOrientation(orientation) {
+  screen.orientation.lock(orientation);
+}
+
+// Function to change screen aspect ratio
+function changeScreenAspectRatio(aspectRatio) {
+  document.documentElement.style.setProperty('--aspect-ratio', aspectRatio);
+}
+
+document.getElementById("portraitBtn").addEventListener("click", changeScreenOrientation("portrait-primary"));
+document.getElementById("landscapeBtn").addEventListener("click", changeScreenOrientation("landscape-primary"));
+document.getElementById("sixteenNineBtn").addEventListener("click", changeScreenAspectRatio("16/9"));
+document.getElementById("sixteenNineBtn").addEventListener("click", changeScreenAspectRatio("4/3"));
